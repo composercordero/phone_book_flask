a1490fdb4f9eb1834151d5de9b3f8e5582e04b4e
composercordero <composercordero@gmail.com>
2023-08-17 01:06:08 -0500

    login and logout

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms import PasswordField, EmailField
-from wtforms.validators import InputRequired
-from wtforms.validators import EqualTo
+from wtforms.validators import InputRequired, EqualTo
+# from wtforms.validators import EqualTo
 
 class SignUpForm(FlaskForm):
+    first_name = StringField('First Name', validators=[InputRequired()])
+    last_name = StringField('Last Name', validators=[InputRequired()])
+    username = StringField('Username', validators=[InputRequired()])
+    email = EmailField('Email', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    confirm_pass = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class ContactForm(FlaskForm):
     first_name = StringField('First Name', validators = [InputRequired()])
     last_name = StringField('Last Name', validators = [InputRequired()])
     phone = StringField('Phone', validators = [InputRequired()])
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app, db
 from flask import render_template, redirect, url_for, flash
-from app.forms import SignUpForm, LoginForm
-from app.models import Contact
+from app.forms import ContactForm, SignUpForm, LoginForm
+from app.models import Contact, User
 from flask_login import login_user, logout_user, login_required, current_user
 
 @app.route('/')
@@ -10,7 +10,7 @@ def index():
 
 @app.route('/contact', methods =["GET", "POST"])
 def contact():
-    form = SignUpForm()
+    form = ContactForm()
     if form.validate_on_submit():
         first_name = form.first_name.data
         last_name = form.last_name.data
@@ -22,10 +22,10 @@ def contact():
             flash('A user with that username already exists', 'danger')
             return redirect(url_for('contact'))
     
-        new_contact = Contact(first_name = first_name, last_name = last_name, phone = phone, address = address)
+        new_contact = Contact(first_name = first_name, last_name = last_name, phone = phone, address = address, user_id = current_user.id)
         db.session.add(new_contact)
         db.session.commit()
-        flash(f'{new_contact.username} has been created', 'success')
+        flash(f'{new_contact.first_name} has been created', 'success')
         return redirect(url_for('index'))
     
     return render_template('contact.html', form = form)
@@ -37,7 +37,7 @@ def login():
         username = form.username.data
         password = form.password.data
 
-        user = db.session.execute(db.select(User).where(User.username = username)).scalar()
+        user = db.session.execute(db.select(User).where(User.username==username)).scalar()
         if user is not None and user.check_password(password):
             login_user(user)
             flash("You have successfully logged in" , "success")
@@ -54,3 +54,30 @@ def logout():
     logout_user()
     flash("You have successfully logged out", "success")
     return redirect(url_for('index'))
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        
+        check_user = db.session.execute(db.select(User).where( (User.username==username) | (User.email==email) )).scalar()
+        if check_user:
+            flash('A user with that username already exists', 'danger')
+            return redirect(url_for('signup'))
+        
+        new_user = User(first_name = first_name, last_name = last_name, username = username, email = email, password = password)
+
+        db.session.add(new_user)
+        db.session.commit()
+        flash(f'{new_user.username} has been created', 'success')
+
+        login_user(new_user)
+  
+        return redirect(url_for('index'))
+    
+    return render_template('signup.html', form=form)
