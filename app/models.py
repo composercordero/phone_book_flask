e085addd9e19f21c4fdad7940bb46652e8e295ae
composercordero <composercordero@gmail.com>
2023-08-17 00:27:50 -0500

    create new tables

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
-from app import db
+from app import db, login
 from datetime import datetime
-# from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -9,9 +10,34 @@ class Contact(db.Model):
     phone = db.Column(db.String(15), nullable = False)
     address = db.Column(db.String(100), nullable = False)
     date_created = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     def __repr__(self):
-        return f"< Contact {self.id}|{self.username}>"
+        return f"< Contact {self.id}|{self.username} >"
+    
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    first_name = db.Column(db.String(50), nullable = False)
+    last_name = db.Column(db.String(50), nullable = False)
+    email = db.Column(db.String(75), nullable = False, unique = True)
+    username = db.Column(db.String(75), nullable = False, unique = True)
+    password = db.Column(db.String, nullable = False)
+    date_created = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    contact = db.relationship('Contact', backref = 'author')
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.password = generate_password_hash(kwargs.get('password'))
+
+    def __repr__(self):
+        return f"< User {self.id}|{self.username} >"
+    
+    def check_password(self, password_guess):
+        return check_password_hash(self.password, password_guess)
+    
+    @login.user_loader
+    def load_user(user_id):
+        return db.session.get(User, user_id)
