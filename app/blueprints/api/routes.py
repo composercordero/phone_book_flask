ded6160d7a09931d065afe358b61d044f762eb1d
composercordero <composercordero@gmail.com>
2023-08-18 10:08:35 -0500

    api auth file

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -4,3 +4,11 @@ from app.models import Contact
 from flask import request
 from .auth import basic_auth, token_auth
 
+@api.route('/token')
+@basic_auth.login_required
+def get_token():
+    auth_user = basic_auth.current_user()
+    token = auth_user.get_token()
+    return {'token': auth_user,
+            'token_exp': auth_user.token_expiration}
+
